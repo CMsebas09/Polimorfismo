4f86e0b88f6fa940ac7629debf77fb7c8e65eda0
Sebas <Sebas@DESKTOP-383F7OG>
2024-05-20 21:10:21 -0500

    Agregar archivos de proyecto.

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Animal  // Clase base (padre) 
+{
+    public virtual void animalSound()  // Método virtual para ser sobrescrito
+    {
+        Console.WriteLine("El animal hace un sonido");
+    }
+}
+
+class Gato : Animal  // Clase derivada (hijo) 
+{
+    public override void animalSound()  // Sobrescribe el método animalSound
+    {
+        Console.WriteLine("El gato dice: ¡miau!");
+    }
+}
+
+class Caballo : Animal  // Clase derivada (hijo) 
+{
+    public override void animalSound()  // Sobrescribe el método animalSound
+    {
+        Console.WriteLine("El caballo dice: ¡relincho!");
+    }
+}
+
+class Perro : Animal  // Clase derivada (hijo) 
+{
+    public override void animalSound()  // Sobrescribe el método animalSound
+    {
+        Console.WriteLine("El perro dice: ¡guau!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal miAnimal = new Animal();  // Crea un objeto Animal
+        Animal miGato = new Gato();  // Crea un objeto Gato
+        Animal miCaballo = new Caballo();  // Crea un objeto Caballo
+        Animal miPerro = new Perro();  // Crea un objeto Perro
+
+        miAnimal.animalSound();
+        miGato.animalSound();
+        miCaballo.animalSound();
+        miPerro.animalSound();
+    }
+}
